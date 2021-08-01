d31c69230c58e31f7fbb0a445e298d4ecb98255f
Jason Jun <uxd.jun@gmail.com>
2021-08-01 16:57:54 +1000

    Clean comments

--- src/pages/[uid]/index.js
+++ src/pages/[uid]/index.js
@@ -5,40 +5,11 @@ import Head from "next/head"
 import Image from "next/image"
 import styled, { css } from "styled-components"
 
-// const fetcher = (...args) => fetch(...args).then((res) => res.json())
-
-// const getProfile = (uid) => {
-//   const { data, error } = useSWR(GITHUB_PROFILE_BASE_URI + uid, fetcher)
-
-//   return {
-//     user: data,
-//     isLoading: !error && !data,
-//     isError: error
-//   }
-// }
-
 const Page = () => {
   const router = useRouter()
   const { uid } = router.query
   const [userProfile, status] = useProfile(uid)
   console.log(userProfile)
-
-  // const Profile = () => {
-  //   const { data, error } = useSWR(GITHUB_PROFILE_BASE_URI + uid, fetcher)
-
-  //   if (error) return <div>failed to load</div>
-  //   if (!data) return <div>loading...</div>
-
-  //   console.log(data)
-
-  //   // render data
-  //   return (
-  //     <div>
-  //       hello {data.name} from {data.location}!
-  //     </div>
-  //   )
-  // }
-
   return (
     <>
       <section>
