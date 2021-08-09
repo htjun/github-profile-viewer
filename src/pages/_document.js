ac2d06143587d852db8ad0a0553133eb980edb72
Jason Jun <uxd.jun@gmail.com>
2021-08-09 20:06:32 +1000

    Inter font weights specified

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -10,7 +10,9 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
         </Head>
         <body>
           <Main />
