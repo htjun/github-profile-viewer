4d9b5edf0147c0a0a09d1fd9cb45ddee6ed03869
Jason Jun <uxd.jun@gmail.com>
2021-08-10 15:58:02 +1000

    Add new sections to Bio

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -1,5 +1,72 @@
-import styled from "styled-components"
-import { Section, BioListWrapper, BioList } from "/styles/styled-elements"
+import styled, { css } from "styled-components"
+import * as style from "/styles/style"
+import { Section } from "/styles/styled-elements"
+
+const BioListWrapper = styled.ul``
+
+const BioList = styled.li`
+  padding: 24px 32px;
+  border-bottom: 1px solid ${style.hsl("grey", 96)};
+  font-size: ${style.fontSize.sm};
+  color: ${style.hsl("neutral", 16)};
+
+  ${(props) =>
+    !props.type &&
+    css`
+      display: grid;
+      grid-template-columns: minmax(120px, 1fr) 2fr;
+
+      h3 {
+        font-weight: ${style.fontWeight.semibold};
+        color: ${style.hsl("neutral", 64)};
+      }
+    `}
+
+  ${(props) =>
+    props.type === "single-col" &&
+    css`
+      display: block;
+    `}
+
+  ${(props) =>
+    props.type === "header" &&
+    css`
+      display: flex;
+      align-items: center;
+      padding: 32px;
+
+      img.avatar {
+        width: 60px;
+        height: 60px;
+        border-radius: 50%;
+        margin-right: 16px;
+      }
+
+      .name-and-id {
+        h1 {
+          font-size: ${style.fontSize.xl};
+          font-weight: ${style.fontWeight.semibold};
+          letter-spacing: ${style.textLetterSpacing.tight};
+          line-height: ${style.textLineHeight.normal};
+          color: ${style.hsl("neutral", 12)};
+        }
+
+        p {
+          font-size: ${style.fontSize.sm};
+          line-height: ${style.textLineHeight.tight};
+          color: ${style.hsl("neutral", 48)};
+        }
+      }
+    `}
+
+    ${(props) =>
+    props.type === "footer" &&
+    css`
+      display: block;
+      padding: 32px;
+      color: ${style.hsl("neutral", 48)};
+    `}
+`
 
 const BioListContent = (props) => {
   if (props.item) {
@@ -11,6 +78,11 @@ const BioListContent = (props) => {
 
 const Bio = (props) => {
   const { data } = props
+
+  function getCreatedYear(date) {
+    const year = new Date(date).getFullYear()
+    return year
+  }
 
   return (
     <Section>
@@ -34,13 +106,17 @@ const Bio = (props) => {
           <h3>Company</h3>
           <p>{data.company}</p>
         </BioListContent>
-        <BioListContent item={data.twitter_username}>
-          <h3>Twitter</h3>
-          <p>{data.twitter_username}</p>
+        <BioListContent item={data.blog}>
+          <h3>Website</h3>
+          <p>{data.blog}</p>
         </BioListContent>
         <BioListContent item={data.email}>
           <h3>Email</h3>
           <p>{data.email}</p>
+        </BioListContent>
+        <BioListContent item={data.twitter_username}>
+          <h3>Twitter</h3>
+          <p>{data.twitter_username}</p>
         </BioListContent>
         <BioListContent item={data.following}>
           <h3>Following</h3>
@@ -49,6 +125,9 @@ const Bio = (props) => {
         <BioListContent item={data.followers}>
           <h3>Followers</h3>
           <p>{data.followers}</p>
+        </BioListContent>
+        <BioListContent type="footer" item={data.created_at}>
+          <p>GitHub member since {getCreatedYear(data.created_at)}</p>
         </BioListContent>
       </BioListWrapper>
     </Section>
