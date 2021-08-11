9cfa008a5d124091077e30a942628ef6daf17857
Jason Jun <uxd.jun@gmail.com>
2021-08-11 10:50:42 +1000

    Card shadow from color to opacity

--- styles/styled-elements.js
+++ styles/styled-elements.js
@@ -4,5 +4,5 @@ import * as style from "/styles/style"
 export const Section = styled.section`
   background-color: #fff;
   border-radius: 8px;
-  box-shadow: 0 24px 32px -24px ${style.hsl('navy', 88)};
+  box-shadow: 0 24px 32px -24px rgba(36, 39, 46, 0.2);
 `
